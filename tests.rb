9a176cf994159a6c6a25d9e15016c6b3af5f07ac
Mason F. Matthews <mason.f.matthews@gmail.com>
2016-02-17 15:13:24 -0500

    Initial commit.

---
+++ tests.rb
@@ -0,0 +1,28 @@
+# Basic test requires
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# Include both the migration and the app itself
+require './migration'
+require './application'
+
+# Overwrite the development database connection with a test connection.
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'test.sqlite3'
+)
+
+# Gotta run migrations before we can run tests.  Down will fail the first time,
+# so we wrap it in a begin/rescue.
+begin ApplicationMigration.migrate(:down); rescue; end
+ApplicationMigration.migrate(:up)
+
+
+# Finally!  Let's test the thing.
+class ApplicationTest < Minitest::Test
+
+  def test_truth
+    assert true
+  end
+
+end
